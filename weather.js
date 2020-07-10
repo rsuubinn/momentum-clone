20e0c373d7a473d859b88ac2f697f294073f90f6
rsuubinn <rsuubinn@gmail.com>
2020-07-10 16:08:36 +0900

    Get temperature and place

---
+++ weather.js
@@ -0,0 +1,58 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = `9373e0fede63843563c3654687d3ae75`;
+
+const COORDS_LS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS_LS, JSON.stringify(coordsObj));
+}
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const placeName = json.name;
+      weather.innerText = `${temperature} @ ${placeName}`;
+    });
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("Can't get current position");
+}
+
+function askCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS_LS);
+  if (loadedCoords === null) {
+    askCoords();
+  } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
